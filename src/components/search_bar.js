827473008216cda4013c07389f77d027ea5f4a95
Chris Barnes <cjbarnes312@gmail.com>
2017-09-19 18:53:14 -0400

    search added and responsive list

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,19 +5,23 @@ class SearchBar extends React.Component {
 	constructor (props){
 		super(props);
 
-		this.state={ term: ''};
+		this.state={ term: '' };
 	}
 
 	render() {
 		return (
-			<div>
+			<div className="search-bar">
 				<input 
 				value={this.state.term}
-				onChange = {e => this.setState({ term: event.target.value})} />
+				onChange = {event => this.onInputChange(event.target.value)} />
 			</div>
 		);
 	}
 
+	onInputChange(term){
+		this.setState({term});
+		this.props.onSearchTermChange(term);
+	}
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -12,21 +13,33 @@ class App extends Component{
 	constructor(props) {
 		super(props);
 
-		this.state = { videos: [] };
-
-		YTSearch({key: API_KEY, term: 'kickflip'}, (videos) => {
-			//this.setState({videos:videos})
-			this.setState({ videos });
-		});
-
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+		//initial search on start up
+		this.videoSearch('kickflip');
+	}
+	//user search
+	videoSearch(term){
+		YTSearch({key: API_KEY, term:term}, (videos) => {
+			this.setState({
+				videos:videos,
+				selectedVideo:videos[0]
+			});
+		});	
 	}
 
 	render(){
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
 		return (
 			<div>
-				<SearchBar />
-				<VideoDetail video={this.state.videos[0]} />
-				<VideoList videos={this.state.videos} />
+				<SearchBar onSearchTermChange={onVideoSelect} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState( {selectedVideo}) }
+					videos={this.state.videos} />
 			</div>
 			);
 	}
