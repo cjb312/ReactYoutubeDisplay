cd1a7200f6c5212a7fd4c8ae8e13ef07ae03fcf3
Chris Barnes <cjbarnes312@gmail.com>
2017-09-19 17:27:49 -0400

    no video showing

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyC_GkU1qR-SdyMGAgvFCG3Kmkfhv15DCto';
+
+
+class App extends Component{
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'kickflip'}, (videos) => {
+			//this.setState({videos:videos})
+			this.setState({ videos });
+		});
+
+	}
+
+	render(){
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.videos[0]} />
+				<VideoList videos={this.state.videos} />
+			</div>
+			);
+	}
+}
+
+
+ReactDom.render(<App />, document.querySelector('.container'));
